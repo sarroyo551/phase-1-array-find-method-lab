5982316fd2e00d29069b91184f622b88124c696b
Santi Arroyo <santi.arroyo551@gmail.com>
2022-06-17 09:33:58 -0400

    commit tree 3d96756675e051c2e1fd4e49e9a77d4a4795751f

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+
+function superbowlWin (arrOfObj) {
+    const foundWin = arrOfObj.find(object => object.result === 'W')
+    if (foundWin === undefined) {
+        return undefined
+    } else {
+        return foundWin.year
+    }
+}
+
+
+
+
+/*function superbowlWin(numOfWins) {
+    let loss = null;
+    for(let result of record){
+        if(numOfWins(result) === true){
+            loss = result;
+        }
+    }
+    console.log('is this working')
+    return loss
+}
+
+function isWin(record){
+    return record.result === 'W'
+}
+
+isWin(2015)*/
